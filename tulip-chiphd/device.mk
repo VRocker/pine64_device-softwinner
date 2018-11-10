a7e29b521335c19f9fc476f5c197f9d250239ab7
Craig Richards <craigr3@hotmail.com>
2018-11-10 15:00:35 +0000

    Include apps from my repo

--- tulip-chiphd/device.mk
+++ tulip-chiphd/device.mk
@@ -2,7 +2,6 @@ $(call inherit-product, device/softwinner/tulip-common/tulip_64_bit.mk)
 $(call inherit-product, device/softwinner/tulip-common/tulip-common.mk)
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/realtek/bluetooth/firmware/rtlbtfw_cfg.mk)
-$(call inherit-product, vendor/supersu/vendor.mk)
 
 KERNEL_SRC_DIR ?= $(ANDROID_BUILD_TOP)/linux/kernel-tulip
 KERNEL_CFG_NAME ?= sun50iw1p1smp_android
@@ -47,10 +46,10 @@ PRODUCT_PACKAGES += \
     ethtool
 
 PRODUCT_PACKAGES += \
-    ESFileExplorer \
     Bluetooth \
-    WIFIADB \
     Camera
+	
+$(call inherit-product, vendor/vrocker/apps/vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
     device/softwinner/tulip-chiphd/overlay
